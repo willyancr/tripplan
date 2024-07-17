0efc57126aa762b5213e1df445ce120cb718bc91
Willyan Costa <willyancr@gmail.com>
2024-07-17 10:42:07 -0300

    Add new Button component with Tailwind CSS variants
    Create a new Button component with Tailwind CSS variants for different styles
    and sizes. This component allows customization of button appearance based on
    the specified variant and size.

---
+++ src/app/components/button.tsx
@@ -0,0 +1,38 @@
+import { tv, VariantProps } from 'tailwind-variants';
+
+const buttonVariants = tv({
+  base: 'flex items-center justify-center gap-2 font-medium px-4 rounded-lg hover:brightness-75 transition-all',
+  variants: {
+    variant: {
+      primary: 'bg-greenish-yellow text-zinc-800',
+      secondary: 'bg-zinc-700 text-zinc-100',
+    },
+    size: {
+      default: 'py-2',
+      full: 'w-full h-12',
+    },
+  },
+  defaultVariants: {
+    variant: 'primary',
+    size: 'default',
+  },
+});
+
+interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  children: React.ReactNode;
+}
+
+export default function Button({
+  children,
+  variant,
+  size,
+  ...props
+}: ButtonProps) {
+  return (
+    <button {...props} className={buttonVariants({ variant, size })}>
+      {children}
+    </button>
+  );
+}
