3aac24256dfe1293cd7438b5f47737b3b978e4c0
Willyan Costa <willyancr@gmail.com>
2024-07-21 22:06:06 -0300

    Remove unnecessary whitespace in CreateTripPage component

--- src/app/(tripplan)/create-trip/page.tsx
+++ src/app/(tripplan)/create-trip/page.tsx
@@ -2,8 +2,6 @@ import InputDestinationAndDate from '@/app/components/create-trip/input-destinat
 import Image from 'next/image';
 
 export default function CreateTripPage() {
-
-  
   return (
     <div className="flex flex-col items-center justify-center h-[600px] max-w-[600px] m-auto gap-10 text-center">
       <div className="flex gap-4 items-center">
