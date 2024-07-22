199cc183e802dd77ee76ef6ebd6020887bcc695c
Willyan Costa <willyancr@gmail.com>
2024-07-21 22:06:33 -0300

    Refactor Guests component to fetch and display participants dynamically
    - Refactored Guests component to fetch participants dynamically from API based
      on trip slug
    - Updated rendering logic to display participants with their confirmation
      status
    - Removed hard-coded guest entries and replaced with dynamic rendering of
      fetched participants

--- src/app/components/trip-details/guests.tsx
+++ src/app/components/trip-details/guests.tsx
@@ -1,31 +1,53 @@
 'use client';
-import ModalConfirmParticipation from './modal-confirm-participation';
 import { useTripDetails } from '@/app/context/trip-details-context';
+import { api } from '@/app/lib/axixos';
 import { CircleCheck, CircleDashed, UserCog2 } from 'lucide-react';
+import { useEffect, useState } from 'react';
 import Button from '../button';
+import ModalConfirmParticipation from './modal-confirm-participation';
 
-export default function Guests() {
+interface Participants {
+  id: string;
+  name: string;
+  email: string;
+  is_confirmed: boolean;
+}
+
+export default function Guests({ params }: { params: { slug: string } }) {
   const { handleButtonManageGuestsOpen, buttonManageGuestsOpen } =
     useTripDetails();
+
+  const [participants, setParticipants] = useState<Participants[]>();
+
+  useEffect(() => {
+    api
+      .get(`http://localhost:3333/trips/${params.slug}/participants`)
+      .then((response) => setParticipants(response.data.participants));
+  }, [params.slug]);
 
   return (
     <div className="flex flex-col gap-6">
       <h2 className="text-2xl"> Convidados</h2>
 
-      <div className="flex items-center justify-between gap-3">
-        <div>
-          <p>Jessica Rabelo</p>
-          <span className="text-sm text-zinc-500">jessicarabelo@gmail.com</span>
-        </div>
-        <CircleDashed />
-      </div>
-      <div className="flex items-center justify-between gap-3">
-        <div>
-          <p>Willyan Costa</p>
-          <span className="text-sm text-zinc-500">willyancr@gmail.com</span>
-        </div>
-        <CircleCheck className="text-lime-300" />
-      </div>
+      {participants &&
+        participants.map((participant, index) => (
+          <div
+            key={participant.id}
+            className="flex items-center justify-between gap-3"
+          >
+            <div>
+              <p>{participant.name ?? `Convidado ${index}`}</p>
+              <span className="text-sm text-zinc-500">{participant.email}</span>
+            </div>
+
+            {participant.is_confirmed ? (
+              <CircleCheck className="size-5 text-lime-300" />
+            ) : (
+              <CircleDashed className="size-5 " />
+            )}
+          </div>
+        ))}
+
       <Button variant="secondary" onClick={handleButtonManageGuestsOpen}>
         <UserCog2 className="size-4" />
         Gerenciar convidados
