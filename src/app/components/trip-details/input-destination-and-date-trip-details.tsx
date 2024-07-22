a7c241a386fdb66c1d2f20a6ab9895596535d5eb
Willyan Costa <willyancr@gmail.com>
2024-07-21 22:06:45 -0300

    Refactor input destination and date component
    Update component to fetch trip details and display destination and date dynamically.

--- src/app/components/trip-details/input-destination-and-date-trip-details.tsx
+++ src/app/components/trip-details/input-destination-and-date-trip-details.tsx
@@ -1,28 +1,47 @@
-import { useTripDetails } from '@/app/context/trip-details-context';
+'use client';
+import { api } from '@/app/lib/axixos';
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { Calendar, MapPin, Settings2 } from 'lucide-react';
+import { useEffect, useState } from 'react';
 import Button from '../button';
 
-interface ParamsProps {
-  params: {
-    slug: string;
-  };
+interface Trip {
+  id: string;
+  destination: string;
+  starts_at: string;
+  ends_at: string;
+  is_confirmed: boolean;
 }
 
-export default async function InputDestinationAndDateTripDetails({
+export default function InputDestinationAndDateTripDetails({
   params,
-}: ParamsProps) {
-  const { getInfosTrip } = useTripDetails();
-  const { trip } = await getInfosTrip(params.slug);
+}: {
+  params: { slug: string };
+}) {
+  const [trip, setTrip] = useState<Trip | undefined>();
+
+  useEffect(() => {
+    api
+      .get(`http://localhost:3333/trips/${params.slug}`)
+      .then((response) => setTrip(response.data.trip));
+  }, [params.slug]);
+
+  const displayDate = trip
+    ? format(trip.starts_at, "dd 'de' MMM", { locale: ptBR })
+        .concat(' até ')
+        .concat(format(trip.ends_at, "dd 'de' MMM"))
+    : null;
 
   return (
     <div className="flex gap-2 bg-zinc-800 px-4 py-2 rounded-lg text-zinc-400 drop-shadow-2x">
       <div className="flex items-center gap-2 flex-1">
         <MapPin className="size-5" />
-        <span>{trip.destination}</span>
+        <span>{trip?.destination}</span>
       </div>
-      <div className="flex items-center gap-2 ">
+      <div className="flex items-center gap-2 flex-1">
         <Calendar className="size-5" />
-        <span>teste</span>
+        <span>{displayDate}</span>
       </div>
       <Button variant="secondary">
         Alterar local/data
