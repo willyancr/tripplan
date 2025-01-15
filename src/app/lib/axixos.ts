c9bdd6a5dcc628cef6ff832320cdb3158fa3db5e
Willyan Costa <willyancr@gmail.com>
2025-01-15 14:48:30 -0300

    chore: update API base URL to development environment

--- src/app/lib/axixos.ts
+++ src/app/lib/axixos.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://api-nodejs-tripplan-production.up.railway.app",
+  baseURL: "https://api-nodejs-tripplan.fly.dev/",
 });
